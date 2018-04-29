f3d3da82f03a273ae0bed75c9a1735a0266fd323
eminguyen <emilyhuongnguyen@gmail.com>
2018-04-29 13:04:37 -0700

    Add animated arrow

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 $('.ui .item').on('click', function() {
       $('.ui .item').removeClass('active');
       $(this).addClass('active');
-   }); 
+   });
+
+$('.arrowthingy')
+  .transition('set looping')
+  .transition('set n')
+  .transition('bounce', '2000ms')
